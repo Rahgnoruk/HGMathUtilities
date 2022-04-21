41c7d6c3ed6e50c8beb7b652084c1d07b79440f9
Rahgnoruk <tono.raimond@hotmail.com>
2022-04-21 12:54:25 -0500

    Agregue FloatOperations
    De momento solo tiene GetPercentage

---
+++ Runtime/FloatOperations.cs
@@ -0,0 +1,10 @@
+namespace HyperGnosys.MathUtilities
+{
+    public static class FloatOperations
+    {
+        public static float GetPercentage(float fraction, float whole)
+        {
+            return fraction / whole;
+        }
+    }
+}
